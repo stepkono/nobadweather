fc695255a36e683125ff381ee9a22d93637bf0a2
stepkonoe <myewadrs@gmail.comm>
2024-10-09 20:38:10 +0200

    Initial Commit

---
+++ NoBadWeatherTests/NoBadWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoBadWeatherTests.swift
+//  NoBadWeatherTests
+//
+//  Created by Stepan Konoplianko on 09.10.24.
+//
+
+import Testing
+@testable import NoBadWeather
+
+struct NoBadWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
